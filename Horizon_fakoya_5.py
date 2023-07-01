1d0af29b375ec7ff162659dac6eab752438b54d6
Horizon <horizonfakoya@hotmail.com>
2023-07-01 08:54:33 -0400

    first commit

---
+++ Horizon_fakoya_5.py
@@ -0,0 +1,6 @@
+people = ['Jane', 'John', 'Jack', 'Janet']
+sex = ['Female', 'Male', 'Male', 'Female']
+age = [23, 34, 16, 28]
+
+person1 = "{people} the {sex} is {age} years old".format(people=people, sex=sex, age=age)
+print(person1)
